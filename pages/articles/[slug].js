60a22abd47e6df607a94410e9463955217a601cd
Valentin <valentin.petit.95@hotmail.fr>
2022-05-04 07:54:57 +0200

    modif

---
+++ pages/articles/[slug].js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Article() {
+
+	return (
+		<>
+			<Head>
+				<title>Article en question</title>
+			</Head>
+		</>
+	);
+}
---
+++ pages/articles/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import React from "react";
+
+export default function Articles() {
+	return (
+		<>
+			<Head>
+				<title>Mes articles</title>
+			</Head>
+			<h1>Mes articles</h1>
+		</>
+	);
+}
