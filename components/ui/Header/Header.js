5e2f4c90225da033161f2fea2da263538773b5df
Valentin <valentin.petit.95@hotmail.fr>
2022-05-05 05:06:46 +0200

    creation fix

--- components/ui/Header/Header.js
+++ components/ui/Header/Header.js
@@ -1,78 +1,63 @@
 // Librairies
 import classes from './Header.module.css';
-import Link from 'next/link';
-import { useSession, signOut } from 'next-auth/client';
-import { useRouter } from 'next/router';
 
 export default function Header() {
-	// Variables
-	const router = useRouter();
-	const [session, loading] = useSession();
 
-	return (
-		<header className={classes.Header}>
-			<div
-				className='container'
-				style={{
-					display: 'flex',
-					justifyContent: 'space-between',
-					alignItems: 'center',
-				}}
-			>
-				<h1 style={{ margin: 0 }}>Blog</h1>
-				<nav>
-					<ul
-						style={{
-							display: 'flex',
-							listStyleType: 'none',
-							margin: 0,
-							padding: 0,
-							gap: '15px',
-						}}
-					>
-						<li>
-							<a href='/'>Accueil</a>
-						</li>
-						<li>
-							<a href='/articles'>Articles</a>
-						</li>
+    return (
+        <header className={classes.Header}>
+            <div
+                className='container'
+                style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}
+            >
+                <h1 style={{margin: 0}}>Blog</h1>
+                <nav>
+                    <ul
+                        style={{
+                            display: 'flex',
+                            listStyleType: 'none',
+                            margin: 0,
+                            padding: 0,
+                            gap: '15px',
+                        }}
+                    >
+                        <li>
+                            <a href='/'>Accueil</a>
+                        </li>
+                        <li>
+                            <a href='/articles'>Articles</a>
+                        </li>
 
-						{!session && !loading && (
-							<>
-								<li>
-									<a href='/connexion'>
-										Connexion
-									</a>
-								</li>
-								<li>
-									<a href='/inscription'>
-										Inscription
-									</a>
-								</li>
-							</>
-						)}
-						{session &&
-							session.user.roles.includes(
-								'administrateur',
-							) && (
-								<li>
-									<a href='/ajouter'>
-										Ajouter
-									</a>
-								</li>
-							)}
-						{session && (
-							<li>
-								<a
-									style={{ cursor: 'pointer' }}
-								>
-									Déconnexion
-								</a>
-							</li>
-						)}
-					</ul>
-				</nav>
-			</div>
-		</header>
-	);
+                        <>
+                            <li>
+                                <a href='/connexion'>
+                                    Connexion
+                                </a>
+                            </li>
+                            <li>
+                                <a href='/inscription'>
+                                    Inscription
+                                </a>
+                            </li>
+                        </>
+                        <li>
+                            <a href='/ajouter'>
+                                Ajouter
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                style={{cursor: 'pointer'}}
+                            >
+                                Déconnexion
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,19 @@
 // Librairie
 import '../styles/default.css';
 import Head from 'next/head';
-import { Provider } from 'next-auth/client';
 
 // Composant
 import Layout from '../components/ui/Layout/Layout';
 
-function MyApp({ Component, pageProps }) {
-	return (
-		<Provider session={pageProps.session}>
-			<Layout>
-				<Head>
-					<title>Blog</title>
-				</Head>
-				<Component {...pageProps} />
-			</Layout>
-		</Provider>
-	);
+function MyApp({Component, pageProps}) {
+    return (
+        <Layout>
+            <Head>
+                <title>Blog</title>
+            </Head>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
 export default MyApp;
